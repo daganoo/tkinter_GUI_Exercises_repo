227cd0f4974ee9cc4c9d7b108ee82a3578ae62b4
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2023-10-03 01:31:47 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+from tkinter import *
+root=Tk()
+#crating a label widget
+myLabel= Label(root,text="nik mha lmodowana")
+#shoving it into the window or screen ma3lina
+myLabel.pack()
+root.mainloop()
