d723bf710cdef91d9f03d29e5a1ea1daca0229f9
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2023-10-04 23:50:31 +0100

    add some functions

--- MyTests/basic_calculator.py
+++ MyTests/basic_calculator.py
@@ -1,14 +1,30 @@
 from tkinter import *
 root = Tk()
 
+# define functions
+def button_clicked(number):
+    curent = e.get()
+    e.delete(0,END)
+    e.insert(0,str(curent)+ str(number))
 
-def button_clicked():
-    return
+def button_clear():
+    e.delete(0,END)
 
+def button_add():
+    first_number = e.get()
+    global f_num
+    f_num = int(first_number)
+    e.delete(0,END)
+
+def button_equal():
+    second_number = e.get()
+    e.delete(0,END)
+    e.insert(0,f_num + int(second_number))
 
 root.title("simple calculator")
 e = Entry(root, width=35, borderwidth=3.5)
 
+# define buttons
 button_1 = Button(root, text=1, padx=40, pady=20, command= lambda : button_clicked(1))
 button_2 = Button(root, text=2, padx=40, pady=20, command= lambda : button_clicked(2))
 button_3 = Button(root, text=3, padx=40, pady=20, command= lambda : button_clicked(3))
@@ -19,11 +35,9 @@ button_7 = Button(root, text=7, padx=40, pady=20, command= lambda : button_click
 button_8 = Button(root, text=8, padx=40, pady=20, command= lambda : button_clicked(8))
 button_9 = Button(root, text=9, padx=40, pady=20, command= lambda : button_clicked(9))
 button_0 = Button(root, text=0, padx=40, pady=20, command= lambda : button_clicked(0))
-button_add = Button(root, text="+", padx=40, pady=20, command= lambda : button_clicked())
-button_eq = Button(root, text="=", padx=91, pady=20, command= lambda : button_clicked())
-button_clear = Button(root, text="c", padx=91, pady=20, command= lambda : button_clicked())
-
-
+button_add = Button(root, text="+", padx=40, pady=20, command= button_add)
+button_equal = Button(root, text="=", padx=91, pady=20, command= button_equal)
+button_clear = Button(root, text="c", padx=91, pady=20, command = button_clear)
 
 button_1.grid(row=3, column=0)
 button_2.grid(row=3, column=1)
@@ -40,7 +54,7 @@ button_9.grid(row=1, column=2)
 button_0.grid(row=4, column=0)
 button_clear.grid(row=4, column=1,columnspan=2)
 button_add.grid(row=5, column=0)
-button_eq.grid(row=5, column=1,columnspan=3)
+button_equal.grid(row=5, column=1,columnspan=3)
 
 
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
