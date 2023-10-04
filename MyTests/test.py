46e06d574873f7d31a66fb236df9cd94124c89ec
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2023-10-04 23:05:28 +0100

    working on basic calculator

--- MyTests/test.py
+++ MyTests/test.py
@@ -1,13 +1,5 @@
 from tkinter import *
 root=Tk()
 
-def myC():
-    mybb=Label(root ,text="Daganoooo")
-    mybb.pack()
 
-#crating a label widget
-myB = Button(root,text="Marouane ", command=myC)
-
-#shoving it into the window or screen ma3lina
-myB.pack()
 root.mainloop()
