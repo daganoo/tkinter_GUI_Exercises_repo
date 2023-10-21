1884ecae1faa220db884d8624030652f85dd472c
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2023-10-21 09:42:34 +0100

    chenges on file

--- MyTests/pint.py
+++ MyTests/pint.py
@@ -1,11 +1,9 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.title("Bigger Point in tkinter")
+window = tk.Tk()
+window.title("Bigger Point in tkinter")
 
-canvas = tk.Canvas(root, width=400, height=400)
-canvas.pack()
+canvas = tk.Canvas(window, width=400, height=400)
 
-canvas.create_oval(195, 195, 50, 250, fill="red")
 
-root.mainloop()
+window.mainloop()
