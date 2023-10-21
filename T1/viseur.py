84fabaecd72fdfed7b327a79875b76d4010e0608
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2023-10-21 12:05:07 +0100

    add viseur file and edit damier

---
+++ T1/viseur.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from random import randrange
+
+def drawline():
+    global x1, y1, x2, y2, cou1
+    can1.create_line(x1, y1, x2, y2, width=1, fill=cou1)
+    y2, y1 = y2 + 10, y1 - 10
+
+def changecolor():
+    global cou1
+    pal = ['blue', 'yellow', 'green','red']
+    c = randrange(4)
+    cou1 = pal[c]
+
+x1, y1, x2, y2 = 10, 190, 190, 10
+cou1 = 'dark green'
+
+def drawline_2():
+    can1.create_line(100, 0, 100, 200, fill='red', width=1)
+    can1.create_line(0,100, 200, 100, fill='red', width=1)
+
+window = Tk()
+can1 = Canvas(window, bg='grey', height=200, width=200)
+can1.pack(side=LEFT)
+bou1 = Button(window, text='Quitter', command=window.quit)
+bou1.pack(side=BOTTOM)
+bou2 = Button(window, text='Tracer ligne', command=drawline)
+bou2.pack()
+bou3 = Button(window, text='Autre couleur', command=changecolor)
+bou3.pack()
+bou4 = Button(window, text='Viseur +', command=drawline_2)
+bou4.pack()
+window.mainloop()
--- T2/Damier.py
+++ T2/Damier.py
@@ -1,46 +1,29 @@
-import tkinter as tk
+from tkinter import *
 from random import randrange
 
-def draw_damier(event=None):
-    global square_size
-    canvas.delete("all")
-    canvas_width = canvas.winfo_width()
-    canvas_height = canvas.winfo_height()
-    square_size = min(canvas_width // columns, canvas_height // rows)
+case_size = 40
+def damier():
+    can.delete('all')
+    for row in range(10):
+        for col in range(10):
+            x1 = col * case_size
+            y1 = row * case_size
+            x2 = x1 + case_size
+            y2 = y1 + case_size
+            couleur = "blue" if (row + col) % 2 == 0 else "white"
 
-    for row in range(rows):
-        for col in range(columns):
-            color_index = (row + col) % 2
-            x0, y0 = col * square_size, row * square_size
-            x1, y1 = x0 + square_size, y0 + square_size
-            canvas.create_rectangle(x0, y0, x1, y1, fill=colors[color_index])
+            can.create_rectangle(x1, y1, x2, y2, fill=couleur, outline="black")
+def pions():
+    i = randrange(10)
+    j = randrange(10)
+    can.create_oval(i*case_size, j*case_size, (i+1)*case_size, (j+1)*case_size, fill="black")
 
-def add_pawn():
-    i = randrange(columns)
-    j = randrange(rows)
-    x0, y0 = i * square_size, j * square_size
-    x1, y1 = x0 + square_size, y0 + square_size
-    canvas.create_oval(x0, y0, x1, y1, fill="black")
 
-root = tk.Tk()
-root.title("Damier")
 
-canvas = tk.Canvas(root)
-canvas.pack(fill="both", expand=True)
+window = Tk()
+can = Canvas(window, height=400, width=400)
+can.pack()
 
-rows, columns, square_size = 10, 10, 40
-colors = ["white", "blue"]
-
-draw_button = tk.Button(root, text="Damier", command=draw_damier)
-draw_button.pack(side="left")
-
-pawn_button = tk.Button(root, text="Dame", command=add_pawn)
-pawn_button.pack(side="left")
-
-quit_button = tk.Button(root, text="Quit", command=root.quit)
-quit_button.pack(side="right")
-
-# Bind canvas to resize event
-canvas.bind("<Configure>", draw_damier)
-
-root.mainloop()
+Button(window, text="Damier", command=damier).pack(side=LEFT)
+Button(window, text="pions", command=pions).pack(side=RIGHT)
+window.mainloop()
