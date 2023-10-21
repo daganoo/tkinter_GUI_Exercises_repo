5d33a41a0934929177fab91e2d9abc1278fd4123
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2023-10-21 11:59:34 +0100

    sm modifications

--- MyTests/pint.py
+++ MyTests/pint.py
@@ -1,9 +1,16 @@
-import tkinter as tk
+from tkinter import *
 
-window = tk.Tk()
+window = Tk()
 window.title("Bigger Point in tkinter")
 
-canvas = tk.Canvas(window, width=400, height=400)
+canvas = Canvas(window, width=700, height=500)
+canvas.pack()
+canvas.create_oval((50,50,200,200))
+canvas.create_oval((200,50,350,200))
+canvas.create_oval((350,50,500,200))
+canvas.create_oval((125,125,275,275))
+canvas.create_oval((275,125,425,275))
 
-
+b1 = Button(window, text="drow the circle")
+b1.pack()
 window.mainloop()
--- MyTests/test.py
+++ MyTests/test.py
@@ -1,4 +1,11 @@
 from tkinter import *
 root=Tk()
+canva=Canvas(root, width=500, height=500)
+canva.pack()
+canva.create_line((250,0,250,500))
+canva.create_line((0,250,500,250))
 
+canva.create_oval((200,200,300,300))
+canva.create_oval((100,100,400,400))
+canva.create_oval((150,150,180,180))
 root.mainloop()
