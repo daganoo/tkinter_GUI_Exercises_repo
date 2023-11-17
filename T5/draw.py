184b348907d150f6a827f84165ac7d241aae9142
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2023-11-18 00:50:21 +0100

    Update draw.py

--- T5/draw.py
+++ T5/draw.py
@@ -1,11 +1,10 @@
-import tkinter as tk
-
+from tkinter import*
 class Application:
     def __init__(self, wnd):
         self.wnd = wnd
         self.wnd.title("draw ")
 
-        self.canvas = tk.Canvas(wnd, width=420, height=300, bg="white")
+        self.canvas = Canvas(wnd, width=420, height=300, bg="white")
         self.canvas.pack()
 
         self.canvas.bind("<B1-Motion>", self.dessiner_ligne)
@@ -14,7 +13,7 @@ class Application:
         x, y = event.x, event.y
         self.canvas.create_line(x, y, x+1, y+1, fill="blue", width=2)
 
-wnd = tk.Tk()
+wnd = Tk()
 app = Application(wnd)
 wnd.mainloop()
 
